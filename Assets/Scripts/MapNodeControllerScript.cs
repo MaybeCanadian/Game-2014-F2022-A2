2e34d50b02ee8b5b76b5b090c801912c1831a475
MaybeCanadian <1coffeye@gmail.com>
2022-10-21 13:15:53 -0400

    started work on nodes for enemy pathfinding

---
+++ Assets/Scripts/MapNodeControllerScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*-----------------------------------
+ * MapNodeControllerScript.cs - Evan Coffey - 101267129
+ * 
+ * Keeps track of all the nodes as well as the order of them all, then enemies can use it to find the path
+ * 
+ * Version History -
+ * 10/21/2022 - created script
+ * 
+ * Latest Revision -
+ * 10/21/2022
+ * ----------------------------------
+ */
+public class MapNodeControllerScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/MapNodeScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*-------------------------------------------------
+ * MapNodeScript.cs - Evan Coffey - 101267129
+ * 
+ * Adds itself to the MapNodeManager for the level
+ * Used for enemy Pathfinding
+ * 
+ * Version History -
+ * 10/21/2022 - created script
+ * 
+ * Latest Revision -
+ * 10/21/2022
+ * ------------------------------------------------
+ */
+
+public class MapNodeScript : MonoBehaviour
+{
+    
+}
